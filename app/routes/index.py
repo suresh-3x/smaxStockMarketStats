218ce2eedfd094da11dff13a5416802c4d78a538
suresh-3x <suresh.37x@gmail.com>
2021-07-28 17:12:10 +0530

    Homepage completed

--- app/routes/index.py
+++ app/routes/index.py
@@ -1,5 +1,6 @@
 from app import app
 from flask import render_template
+import pandas as pd
 
 @app.route("/")
 def index():
@@ -7,4 +8,7 @@ def index():
 
 @app.route("/home")
 def home():
-    return render_template("home.html")
+    df = pd.read_csv('app/data/Equity.csv')
+    df = df.drop(['Face Value', 'Instrument', 'Group', 'Industry', 'Security Name', "ISIN No"], axis=1)
+    df = df.to_dict(orient='records')
+    return render_template("home.html", data=df)
--- app/static/js/index.js
+++ app/static/js/index.js
@@ -1,33 +1,85 @@
-(function () {
-    const second = 1000,
-          minute = second * 60,
-          hour = minute * 60,
-          day = hour * 24;
-  
-    let birthday = "July 31, 2021 00:00:00",
-        countDown = new Date(birthday).getTime(),
-        x = setInterval(function() {    
-  
-          let now = new Date().getTime(),
-              distance = countDown - now;
-  
-          document.getElementById("days").innerText = Math.floor(distance / (day)),
-            document.getElementById("hours").innerText = Math.floor((distance % (day)) / (hour)),
-            document.getElementById("minutes").innerText = Math.floor((distance % (hour)) / (minute)),
-            document.getElementById("seconds").innerText = Math.floor((distance % (minute)) / second);
-  
-          //do something later when date is reached
-          if (distance < 0) {
-            let headline = document.getElementById("headline"),
-                countdown = document.getElementById("countdown"),
-                content = document.getElementById("content");
-  
-            headline.innerText = "It's my birthday!";
-            countdown.style.display = "none";
-            content.style.display = "block";
-  
-            clearInterval(x);
-          }
-          //seconds
-        }, 0)
-    }());
+var stocksData = [];
+
+$(document).ready(function() {
+    // executes when HTML-Document is loaded and DOM is ready
+    getStocksData()
+   });
+
+
+function getStocksData(){
+    let array = new Array;
+    let fetches = []
+    for(let stock of STOCKS){
+        fetches.push(fetch(`https://query1.finance.yahoo.com/v8/finance/chart/${stock['Issuer Name']}.BO?region=IN&lang=en-IN`)
+        .then(res=>{
+            if (res.status != 200){
+                return Promise.reject(400)
+            }
+            else{
+                return res.json()
+            }
+        })
+        .then(data=>{
+            data['Name'] = stock['Security Code']
+            data['Symbol'] = stock['Issuer Name']
+            stocksData.push(data)
+        })
+        .catch(err=>{
+            console.log(err)
+        }))
+    }
+    
+    Promise.all(fetches).then(function() {
+        renderStockData()
+    });
+}
+
+function renderStockData(){
+    document.querySelector('#load').remove()
+    document.querySelector('.row').innerHTML = `
+    <div class="col-lg-4">
+    <div class="card">
+        <div class="card-header">
+            <h3 class="card-title">Top 10 Movers</h3>
+        </div>
+        <div class="card-body">
+            <div class="table-responsive">
+                <table class="table">
+                    <thead class="text-primary">
+                        <tr>
+                            <th>Symbol</th>
+                            <th>Name</th>
+                            <th>Price</th>
+                            <th>Change</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+    </div>`
+    for (let stock of stocksData) {
+        if (stock['chart']['result'][0]['meta']['regularMarketPrice'] == undefined || stock['chart']['result'][0]['meta']['previousClose'] == undefined){
+            stocksData.splice(stocksData.indexOf(stock),1)      
+        }
+    }
+    for (let stock of stocksData){
+        let priceChange = stock['chart']['result'][0]['meta']['regularMarketPrice'] - stock['chart']['result'][0]['meta']['previousClose']
+        let percentChange = parseFloat(((priceChange.toFixed(2) / stock['chart']['result'][0]['meta']['previousClose']) * 100).toFixed(2))
+        stock['Percent Change'] = percentChange
+    }
+    stocksData.sort(function(a, b){return a['Percent Change'] - b['Percent Change']});
+    for(let stock of stocksData.slice(-10)){
+        let tr = document.createElement("tr")
+        tr.innerHTML = `
+        <td>${stock['Symbol']}</td>
+        <td>${stock['Name']}</td>
+        <td>${stock['chart']['result'][0]['meta']['regularMarketPrice']}</td>
+        <td>${stock['Percent Change']}</td>`
+        document.querySelector('.row tbody').appendChild(tr)
+
+    }
+}
