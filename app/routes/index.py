31f72dfa3257bd5d8f6f601f8e0959c1c74048c2
suresh-3x <suresh.37x@gmail.com>
2021-07-29 13:17:24 +0530

    added stock fetching in the backend

--- app/routes/index.py
+++ app/routes/index.py
@@ -1,14 +1,21 @@
 from app import app
-from flask import render_template
+from flask import render_template, request
 import pandas as pd
+from services.bse import get_top_10_movers
 
 @app.route("/")
 def index():
     return render_template("index.html")
 
-@app.route("/home")
-def home():
-    df = pd.read_csv('app/data/Equity.csv')
-    df = df.drop(['Face Value', 'Instrument', 'Group', 'Industry', 'Security Name', "ISIN No"], axis=1)
-    df = df.to_dict(orient='records')
-    return render_template("home.html", data=df)
+@app.route("/home", methods=['GET', 'POST'])
+async def home():
+    if request.method == "POST":
+        losers, gainers = await get_top_10_movers()
+        data = {
+            'gainers': gainers,
+            'losers': losers
+        }
+        
+        return render_template("home.html", data=data)
+    else:
+        return render_template("home.html")
---
+++ services/bse.py
@@ -0,0 +1,39 @@
+
+import aiohttp
+import asyncio
+import pandas as pd
+
+
+dataset = pd.read_csv(r'app/data/Equity.csv')
+STOCKS = dataset[['Issuer Name', 'Security Code']]
+STOCKS = STOCKS[:500].values.tolist()
+header = {'User': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:76.0) Gecko/20100101 Firefox/76.0'}
+DATA = []
+async def get_stock_data(session, bseid, fullname):
+    global DATA
+    url = f"https://query1.finance.yahoo.com/v8/finance/chart/{bseid}.BO?region=IN&lang=en-IN&includePrePost=false&interval=2m&useYfid=true&range=1d&corsDomain=in.finance.yahoo.com&.tsrc=finance"
+    async with session.get(url) as response:        
+        res = await response.json()
+        try:
+            LTP = res['chart']['result'][0]['meta']['regularMarketPrice']
+            pc = round(float((res['chart']['result'][0]['meta']['regularMarketPrice'] - res['chart']['result'][0]['meta']['previousClose'])
+                        / res['chart']['result'][0]['meta']['previousClose'] * 100 ),2)
+            c = round(float(res['chart']['result'][0]['meta']['regularMarketPrice'] - res['chart']['result'][0]['meta']['previousClose']),2)
+            x = {'Name':bseid,'FullName':fullname,'Rate':LTP,'Change':c,'PChange':pc}
+            DATA.append(x)
+        except Exception as e:
+            pass
+  
+
+async def get_top_10_movers():
+    global DATA
+    async with aiohttp.ClientSession() as session:
+        tasks = []
+        DATA = []
+        for bseid, fullname in STOCKS:
+            task = asyncio.ensure_future(get_stock_data(session, bseid, fullname))
+            tasks.append(task)
+
+        await asyncio.gather(*tasks)
+        stock_list = sorted(DATA, key=lambda k: k['PChange'])
+        return stock_list[:10], stock_list[-10:][::-1]
