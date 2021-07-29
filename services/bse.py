7fb65237ea505ca789c1532c466c6df91fa8a866
suresh-3x <suresh.37x@gmail.com>
2021-07-29 13:34:23 +0530

    fixed styling and removed full name from home table

--- services/bse.py
+++ services/bse.py
@@ -5,11 +5,11 @@ import pandas as pd
 
 
 dataset = pd.read_csv(r'app/data/Equity.csv')
-STOCKS = dataset[['Issuer Name', 'Security Code']]
-STOCKS = STOCKS[:500].values.tolist()
+STOCKS = dataset['Issuer Name']
+STOCKS = STOCKS[:500]
 header = {'User': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:76.0) Gecko/20100101 Firefox/76.0'}
 DATA = []
-async def get_stock_data(session, bseid, fullname):
+async def get_stock_data(session, bseid):
     global DATA
     url = f"https://query1.finance.yahoo.com/v8/finance/chart/{bseid}.BO?region=IN&lang=en-IN&includePrePost=false&interval=2m&useYfid=true&range=1d&corsDomain=in.finance.yahoo.com&.tsrc=finance"
     async with session.get(url) as response:        
@@ -19,7 +19,7 @@ async def get_stock_data(session, bseid, fullname):
             pc = round(float((res['chart']['result'][0]['meta']['regularMarketPrice'] - res['chart']['result'][0]['meta']['previousClose'])
                         / res['chart']['result'][0]['meta']['previousClose'] * 100 ),2)
             c = round(float(res['chart']['result'][0]['meta']['regularMarketPrice'] - res['chart']['result'][0]['meta']['previousClose']),2)
-            x = {'Name':bseid,'FullName':fullname,'Rate':LTP,'Change':c,'PChange':pc}
+            x = {'Name':bseid,'Rate':LTP,'Change':c,'PChange':pc}
             DATA.append(x)
         except Exception as e:
             pass
@@ -30,8 +30,8 @@ async def get_top_10_movers():
     async with aiohttp.ClientSession() as session:
         tasks = []
         DATA = []
-        for bseid, fullname in STOCKS:
-            task = asyncio.ensure_future(get_stock_data(session, bseid, fullname))
+        for bseid in STOCKS:
+            task = asyncio.ensure_future(get_stock_data(session, bseid))
             tasks.append(task)
 
         await asyncio.gather(*tasks)
